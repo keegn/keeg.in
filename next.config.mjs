2f3eb137a99caa4d298448e3857973a5c568039d
keegn <keegan.t.burkett@gmail.com>
2023-06-02 20:05:55 -0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  /**
+     * The following items speed up builds and move typechecking to github actions. - see the workflows ci.yml 
+     * must comment the below `i18n` config out.
+     *
+     /** */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+};
+
+export default nextConfig;
