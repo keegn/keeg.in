11a163ddf19261c97a1ae9a81ce9a8dc7c042125
keegn <keegan.t.burkett@gmail.com>
2023-06-23 15:59:29 -0500

    completes super mvp

--- components/sections/product-card.tsx
+++ components/sections/product-card.tsx
@@ -7,17 +7,19 @@ export function ProductCard() {
   return (
     <Container type="section" className="px-4 md:px-0 md:pb-12">
       <ul className="flex flex-col">
-        <li className="flex">
-          <Link
+        <li className=" flex">
+          {/* <Link
             href="https://www.example.com"
             target="_blank"
-            className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
-          >
+            className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:cursor-not-allowed hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
+            aria-disabled="true"
+          > */}
+          <div className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:cursor-not-allowed hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900">
             {/* Icon */}
             <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md border-transparent bg-zinc-100 p-2 dark:bg-zinc-900">
               <Image
                 src="/design/framer.webp"
-                alt="vscode logo"
+                alt="Framer logo"
                 width={36}
                 height={36}
               />
@@ -27,14 +29,16 @@ export function ProductCard() {
               <span className="font-bold">Framer Template</span>
               <span className="text-secondary-foreground">Coming soon</span>
             </div>
-          </Link>
+          </div>
+          {/* </Link> */}
         </li>
         <li className="flex">
-          <Link
+          {/* <Link
             href="https://www.example.com"
             target="_blank"
             className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
-          >
+          > */}
+          <div className="flex w-full items-center gap-5  p-5 hover:cursor-not-allowed hover:rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900">
             {/* Icon */}
             <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md border-transparent bg-zinc-100 p-2 dark:bg-zinc-900">
               <Image
@@ -49,7 +53,8 @@ export function ProductCard() {
               <span className="font-bold">B2B SaaS</span>
               <span className="text-secondary-foreground">Coming 2023</span>
             </div>
-          </Link>
+          </div>
+          {/* </Link> */}
         </li>
       </ul>
     </Container>
--- components/sections/stack-card.tsx
+++ components/sections/stack-card.tsx
@@ -9,31 +9,7 @@ export function StackCard() {
       <ul className="flex flex-col">
         <li className="flex">
           <Link
-            href="https://www.example.com"
-            target="_blank"
-            className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
-          >
-            {/* Icon */}
-            <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md border-transparent bg-zinc-100 p-2 dark:bg-zinc-900">
-              <Image
-                src="/design/vscode.png"
-                alt="vscode logo"
-                width={36}
-                height={36}
-              />
-            </div>
-            {/* Text */}
-            <div className="flex flex-col items-start">
-              <span className="font-bold">Visual Studio Code</span>
-              <span className="text-secondary-foreground">
-                Code editor of choice
-              </span>
-            </div>
-          </Link>
-        </li>
-        <li className="flex">
-          <Link
-            href="https://www.example.com"
+            href="https://www.figma.com"
             target="_blank"
             className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
           >
@@ -57,7 +33,7 @@ export function StackCard() {
         </li>
         <li className="flex">
           <Link
-            href="https://www.example.com"
+            href="https://www.tailwindcss.com"
             target="_blank"
             className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
           >
@@ -65,7 +41,7 @@ export function StackCard() {
             <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md border-transparent bg-zinc-100 p-2 dark:bg-zinc-900">
               <Image
                 src="/design/tailwind.png"
-                alt="Figma logo"
+                alt="Tailwind CSS logo"
                 width={36}
                 height={36}
               />
@@ -81,7 +57,31 @@ export function StackCard() {
         </li>
         <li className="flex">
           <Link
-            href="https://www.example.com"
+            href="https://code.visualstudio.com/"
+            target="_blank"
+            className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
+          >
+            {/* Icon */}
+            <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md border-transparent bg-zinc-100 p-2 dark:bg-zinc-900">
+              <Image
+                src="/design/vscode.png"
+                alt="vscode logo"
+                width={36}
+                height={36}
+              />
+            </div>
+            {/* Text */}
+            <div className="flex flex-col items-start">
+              <span className="font-bold">Visual Studio Code</span>
+              <span className="text-secondary-foreground">
+                Code editor of choice
+              </span>
+            </div>
+          </Link>
+        </li>
+        <li className="flex">
+          <Link
+            href="https://www.nextjs.org"
             target="_blank"
             className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
           >
@@ -89,7 +89,7 @@ export function StackCard() {
             <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md border-transparent bg-zinc-100 p-2 dark:bg-zinc-900">
               <Image
                 src="/design/nextjs.png"
-                alt="Figma logo"
+                alt="Next.js logo"
                 width={36}
                 height={36}
                 className="fill-white"
@@ -101,6 +101,77 @@ export function StackCard() {
               <span className="text-secondary-foreground">
                 Frontend framework
               </span>
+            </div>
+          </Link>
+        </li>
+        <li className="flex">
+          <Link
+            href="https://www.planetscale.com"
+            target="_blank"
+            className="flex w-full items-center gap-5 border-b border-zinc-100 p-5 hover:rounded-md hover:bg-zinc-100 dark:border-zinc-900 dark:hover:bg-zinc-900"
+          >
+            {/* Icon */}
+            <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md border-transparent bg-zinc-100 p-2 dark:bg-zinc-900">
+              <Image
+                src="/design/planetscale.png"
+                alt="PlanetScale logo"
+                width={36}
+                height={36}
+                className="fill-white"
+              />
+            </div>
+            {/* Text */}
+            <div className="flex flex-col items-start">
+              <span className="font-bold">Planetscale</span>
+              <span className="text-secondary-foreground">
+                Serverless MySQL database
+              </span>
+            </div>
+          </Link>
+        </li>{' '}
+        <li className="hidden dark:flex">
+          <Link
+            href="https://www.openai.com"
+            target="_blank"
+            className="b flex w-full items-center gap-5 p-5 hover:rounded-md hover:bg-zinc-100  dark:hover:bg-zinc-900"
+          >
+            {/* Icon */}
+            <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md  bg-zinc-100 p-2 dark:bg-zinc-900">
+              <Image
+                src="/design/openai-white-logomark.png"
+                alt="Open AI logo"
+                width={36}
+                height={36}
+                className="fill-white"
+              />
+            </div>
+            {/* Text */}
+            <div className="flex flex-col items-start">
+              <span className="font-bold">OpenAI</span>
+              <span className="text-secondary-foreground">LLM API</span>
+            </div>
+          </Link>
+        </li>
+        <li className="flex dark:hidden">
+          <Link
+            href="https://www.openai.com"
+            target="_blank"
+            className="flex w-full items-center gap-5  p-5 hover:rounded-md  hover:bg-zinc-100 dark:hover:bg-zinc-900"
+          >
+            {/* Icon */}
+            <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-md  bg-zinc-100 p-2 dark:bg-zinc-900">
+              <Image
+                src="/design/openai-logomark.png"
+                alt="Open AI logo"
+                width={36}
+                height={36}
+                className="fill-white"
+              />
+            </div>
+            {/* Text */}
+            <div className="flex flex-col items-start">
+              <span className="font-bold">OpenAI</span>
+              <span className="text-secondary-foreground">LLM API</span>
             </div>
           </Link>
         </li>
