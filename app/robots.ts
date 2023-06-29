d762593fea6266f5644d94efdd27e289073bd89e
keegn <keegan.t.burkett@gmail.com>
2023-06-29 15:50:25 -0500

    Adds some opengraph info

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://keeg.in/sitemap.xml',
+    host: 'https://keeg.in',
+  }
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+export default async function sitemap() {
+  const routes = ['', '/', '/stack', '/products'].map((route) => ({
+    url: `https://keeg.in${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes]
+}
