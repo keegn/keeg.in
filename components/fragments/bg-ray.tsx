abcccd91daf338ff15df0947dffe1ffe8ec70830
keegn <keegan.t.burkett@gmail.com>
2023-07-01 13:21:26 -0500

    Fix lighthouse img aspect ration issue

--- components/fragments/bg-ray.tsx
+++ components/fragments/bg-ray.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
+import bgRay from '@/public/design/bgray.png'
 
 export function BgRay() {
   return (
     <Image
-      src="/design/bgray.png"
+      src={bgRay}
       alt="Next.js"
       width={1000}
-      height={1000}
-      className="right-100 pointer-events-none absolute -top-20 left-0 z-0 mx-auto  h-full w-full select-none "
+      className="right-100 pointer-events-none absolute -top-20 left-0 z-0 mx-auto  h-full w-full select-none"
     />
   )
 }
