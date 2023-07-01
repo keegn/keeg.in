7947bfbfc6b48e8ad8d30ab1219c697084533463
keegn <keegan.t.burkett@gmail.com>
2023-07-01 13:22:08 -0500

    Adds better alt text

--- components/fragments/bg-ray.tsx
+++ components/fragments/bg-ray.tsx
@@ -5,7 +5,7 @@ export function BgRay() {
   return (
     <Image
       src={bgRay}
-      alt="Next.js"
+      alt="Background design element ray of light shining down"
       width={1000}
       className="right-100 pointer-events-none absolute -top-20 left-0 z-0 mx-auto  h-full w-full select-none"
     />
