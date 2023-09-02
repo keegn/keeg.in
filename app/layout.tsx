b0582c301a9e87d095aae739ebc08c43eaa4e5c4
keegn <keegan.t.burkett@gmail.com>
2023-09-02 10:38:28 -0500

    Add v1 param

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
         <Analytics />
         <Script
           id="cline"
-          src="https://cdn.jsdelivr.net/npm/cline-client-script/cline.min.js"
+          src="https://cdn.jsdelivr.net/npm/cline-client-script/cline.min.js?v=1"
         />
         <Script id="cline-page-id">
           {`window.EXPERIMENT_PAGE_ID="64ed658c2c5871d93b500ac2";`}
