b9d7a22be4dd04260b757d92d10a9e00dc7c666f
keegn <keegan.t.burkett@gmail.com>
2023-11-12 20:52:19 -0600

    Adds testing

---
+++ app/(marketing)/stack/one/page.tsx
@@ -0,0 +1,10 @@
+import { Hero } from '@/components/sections/hero'
+import { StackCard } from '@/components/sections/stack-card'
+
+export default function One() {
+  return (
+    <>
+      <Hero title={`One`} />
+    </>
+  )
+}
---
+++ app/(marketing)/stack/two/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from '@/components/sections/hero'
+
+export default function Two() {
+  return (
+    <>
+      <Hero title={`Two`} />
+    </>
+  )
+}
